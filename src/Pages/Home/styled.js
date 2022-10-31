c83a6ef00dba114704adf53385e75932b37889ca
Antonio Cláudio Holanda Borges Junior <claudiohbjunior@gmail.com>
2022-10-30 22:11:18 -0300

    Home e modal finalizados

---
+++ src/Pages/Home/styled.js
@@ -0,0 +1,153 @@
+import styled from "styled-components"
+
+export const H1 = styled.h1`
+font-size: 30px;
+
+`
+export const Wrapper = styled.section`
+  padding: 15.5em;
+  background: papayawhip;
+`;
+
+export const Small = styled.small`
+font-size: 12px;
+position: relative;
+color: red;
+`
+
+export const H2 = styled.h1`
+position: absolute;
+top: 70px;
+left: 650px;
+font-size: 20px;
+
+`
+
+export const Div = styled.div`
+display: flex;
+flex-direction: row;
+
+`
+
+export const Container = styled.div`
+display: flex;
+flex-direction: row;
+padding: 160px;
+border: 1px solid;
+
+
+`
+export const Select = styled.select`
+    
+width: 240px;
+padding: 12px 20px;
+margin: 8px 0;
+box-sizing: border-box;
+   
+`
+export const Input = styled.input`
+font-size: px;
+padding: 12px 20px;
+margin: 8px 0;
+box-sizing: border-box;
+
+`
+export const Button = styled.button`
+background-color: #04AA6D;
+border: none;
+color: white;
+padding: 16px 32px;
+text-decoration: none;
+margin: 4px 2px;
+cursor: pointer;
+border-radius: 10px;
+
+`
+
+export const Btn = styled.button`
+background-color: #04AA6D;
+border: none;
+color: white;
+padding: 16px 32px;
+text-decoration: none;
+margin: 4px 2px;
+cursor: pointer;
+border-radius: 10px;
+position: absolute;
+top: 380px;
+left: 209px;
+`
+export const BtnX = styled.button`
+background-color: red;
+border: none;
+color: white;
+padding: 16px 32px;
+text-decoration: none;
+margin: 4px 2px;
+cursor: pointer;
+border-radius: 10px;
+position: absolute;
+top: 380px;
+left: 340px;
+
+`
+
+export const DivInput = styled.div`
+display: flex;
+flex-direction: column;
+padding: 10px;
+margin-top: 10px;
+margin: 10px;
+border-radius: 4px;
+background: #fff;
+width: 290px;
+
+`
+
+export const DivSelect = styled.div`
+display: flex;
+flex-direction: column;
+padding: 10px;
+margin-top: 10px;
+margin: 10px;
+border-radius: 4px;
+background: #fff;
+
+width: 400px;
+
+
+`
+
+export const FormStyled=styled.form`
+  display: flex;
+  flex-direction: row;
+  position: absolute;
+  
+  left: 490px;
+  top: 120px;
+`
+
+export const Span = styled.span`
+color:orange;
+font-size: 2em;
+margin-top: 0.67em;
+margin-bottom: 0.67em;
+margin-left: 0;
+margin-right: 0;
+font-weight: bold;
+`
+export const H3 = styled.span`
+color:#04AA6D;
+font-size: 2em;
+margin-top: 0.67em;
+margin-bottom: 0.67em;
+margin-left: 0;
+margin-right: 0;
+font-weight: bold;
+`
+
+export const Legend = styled.legend`
+color:#04AA6D;
+font-size: 2em;
+
+`
